32b0b9388d1dab36922656585dd2e9027dc84f80
Alex <balkanskiy.alexey@gmail.com>
2024-02-05 11:54:28 +0100

    Process errors in fetch

--- src/stable-components/cards/useCards.jsx
+++ src/stable-components/cards/useCards.jsx
@@ -6,10 +6,15 @@ export const useCards = (numberOfFacts = 6) => {
     useEffect(() => {
         const fetchCatFacts = async () => {
             const facts = [];
-            for(let i=0; i < numberOfFacts; i++){
-                const response = await fetch('https://catfact.ninja/fact');
-                const data = await response.json();
-                facts.push(data.fact);
+            for (let i=0; i < numberOfFacts; i++){
+                try {
+                    const response = await fetch('https://catfact.ninja/fact');
+                    const data = await response.json();
+                    facts.push(data?.fact);
+                } catch (e) {
+                    facts.push('fact');
+                }
+
             }
             setCatFacts(facts);
         };
